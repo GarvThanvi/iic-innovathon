81fa8b6318b543d554b179911a88bdf71aff129a
GarvThanvi <gtuhm2004@gmail.com>
2024-10-14 19:01:57 +0530

    added router pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import LogIn from './pages/LogIn'
+import Community from './pages/Community';
+import SignUp from './pages/SignUp';
+import Appointment from './pages/Appointment';
+import Activities from './pages/Activities';
+
+
 
 function App() {
 
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/community' element={<Community></Community>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/activities' element={<Activities></Activities>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
