e0d1d0dfe8a0e02a32346ee7957f353d33a46e62
VanshShah2902 <vansh.ds@somaiya.edu>
2024-10-14 19:08:24 +0530

    ggs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-
+import styles from "./Home.module.css"
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className ={styles.Name} >Home</div>
+
   )
 }
